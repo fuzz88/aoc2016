3e5f235a21bd82a216ed16724b2b72916771f4d4
fuzz88 <ivan@oschepkov.ru>
2025-04-10 13:05:16 +0800

    [day1] part2 solved

--- day1/p2/main.rs
+++ day1/p2/main.rs
@@ -2,10 +2,11 @@
 // we need to use Manhattan distance. lets figure out coordinates of the the end,
 // then calculate our Manhattan distance from (0, 0)
 
+use std::collections::HashSet;
 use std::fs;
 
 // lets use Point for point coordinates
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
@@ -33,6 +34,7 @@ struct EasterBunnyRecruitingDocument {
     index: usize,
     azimut: i32, //angle, 0 - North, 180 - South, 90 - West, 270 - East
     stand_at: Point,
+    new_instr: bool,
 }
 
 impl EasterBunnyRecruitingDocument {
@@ -56,6 +58,7 @@ impl EasterBunnyRecruitingDocument {
             index: 0,
             azimut: 0,
             stand_at: Point::new(),
+            new_instr: true,
         }
     }
 }
@@ -63,47 +66,59 @@ impl EasterBunnyRecruitingDocument {
 impl Iterator for EasterBunnyRecruitingDocument {
     type Item = Point;
 
+    // let this give us all points in between
     fn next(&mut self) -> Option<Self::Item> {
+        // if this last instruction -- stop iteration
         if self.index == self.path.len() {
             return None;
         }
-        let curr_instr = &self.path[self.index];
-        self.azimut += match curr_instr.dir {
-            Dir::R => 90,
-            Dir::L => -90,
-        };
-        if self.azimut < 0 {
-            self.azimut += 360;
+        let curr_instr = &mut self.path[self.index];
+
+        // exhausing current instruction or this is a new?
+        if self.new_instr {
+            // if new -- lets correct the azimut
+            self.azimut += match curr_instr.dir {
+                Dir::R => 90,
+                Dir::L => -90,
+            };
+            if self.azimut < 0 {
+                self.azimut += 360;
+            }
+            if self.azimut >= 360 {
+                self.azimut -= 360;
+            }
         }
-        if self.azimut >= 360 {
-            self.azimut -= 360;
-        }
+        // move one atomic step towards the give azimut
         match self.azimut {
             0 => {
-                self.stand_at.y -= curr_instr.length as i32;
+                self.stand_at.y -= 1 as i32;
             }
             90 => {
-                self.stand_at.x += curr_instr.length as i32;
+                self.stand_at.x += 1 as i32;
             }
             180 => {
-                self.stand_at.y += curr_instr.length as i32;
+                self.stand_at.y += 1 as i32;
             }
             270 => {
-                self.stand_at.x -= curr_instr.length as i32;
+                self.stand_at.x -= 1 as i32;
             }
             _ => unreachable!(),
         }
-        self.index += 1;
+        // same azimut, smaller length
+        curr_instr.length -= 1;
+        self.new_instr = false;
+        // checks if current instruction is fully exhausted
+        if curr_instr.length == 0 {
+            // if fully exhausted then prepare next instruction
+            self.new_instr = true;
+            self.index += 1;
+        }
         Some(self.stand_at.clone())
     }
 }
 
 fn manhattan_distance(point: Point) -> u32 {
     point.x.abs() as u32 + point.y.abs() as u32
-}
-
-fn find_first_visited(points: Vec<Point>) -> Point {
-    Point::new()
 }
 
 fn main() {
@@ -114,11 +129,14 @@ fn main() {
         .to_string();
 
     for path in input.lines() {
-        let mut points: Vec<Point> = vec![];
-        let mut ebrd = EasterBunnyRecruitingDocument::new(path);
+        let mut points = HashSet::new();
+        let ebrd = EasterBunnyRecruitingDocument::new(path);
         for next_point in ebrd {
-            points.push(next_point);
+            if points.contains(&next_point) {
+                println!("dist = {}", manhattan_distance(next_point));
+                break;
+            }
+            points.insert(next_point);
         }
-        dbg!(points);
     }
 }
