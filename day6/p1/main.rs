9bcb810fada229ffe296041ff11f1690d7fcf2ab
fuzz88 <ivan@oschepkov.ru>
2025-05-21 22:06:46 +0800

    [day6] read input

--- day6/p1/main.rs
+++ day6/p1/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("--- Day 7: * --- ");
+    println!("--- Day 6: Signals and Noise --- ");
+
+    if let Some(input_file) = env::args().nth(1) {
+        let input_data = read_input(&input_file);
+        let message = decode(&input_data);
+        println!("{}", message);
+    } else {
+        eprintln!("ERROR: no input file");
+        process::exit(1);
+    }
+}
+
+fn read_input(filename: &str) -> Vec<String> {
+    fs::read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(|message| message.to_string())
+        .collect()
+}
+
+fn decode(_messages: &Vec<String>) -> String {
+    "123".to_string()
 }
