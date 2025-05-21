7a8a1576f25ce6b959006cf1b1c27946501cbbe9
fuzz88 <ivan@oschepkov.ru>
2025-05-21 23:00:33 +0800

    [day6] solved

--- day6/p1/main.rs
+++ day6/p1/main.rs
@@ -1,14 +1,14 @@
-use std::env;
-use std::fs;
-use std::process;
+use std::{collections::HashMap, env, fs, process};
 
 fn main() {
     println!("--- Day 6: Signals and Noise --- ");
 
     if let Some(input_file) = env::args().nth(1) {
         let input_data = read_input(&input_file);
-        let message = decode(&input_data);
-        println!("{}", message);
+        let messages = decode(&input_data);
+
+        println!("{}", messages.0);
+        println!("{}", messages.1);
     } else {
         eprintln!("ERROR: no input file");
         process::exit(1);
@@ -23,6 +23,36 @@ fn read_input(filename: &str) -> Vec<String> {
         .collect()
 }
 
-fn decode(_messages: &Vec<String>) -> String {
-    "123".to_string()
+fn decode(messages: &Vec<String>) -> (String, String) {
+    let mut counters: Vec<HashMap<char, u32>> = vec![];
+
+    for message in messages {
+        for (idx, ch) in message.chars().enumerate() {
+            if counters.len() < idx + 1 {
+                counters.push(HashMap::new());
+            }
+            let freq = counters[idx].entry(ch).or_insert(0);
+            *freq += 1;
+        }
+    }
+
+    let mut results = (String::new(), String::new());
+
+    for counter in counters {
+        let (most_common_char, _) = counter
+            .iter()
+            .max_by_key(|(_a, b)| **b)
+            .expect("expecting not empty iterator");
+
+        results.0.push(*most_common_char);
+
+        let (least_common_char, _) = counter
+            .iter()
+            .min_by_key(|(_a, b)| **b)
+            .expect("expecting not empty iterator");
+
+        results.1.push(*least_common_char);
+    }
+
+    results
 }
