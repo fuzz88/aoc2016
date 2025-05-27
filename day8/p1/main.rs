41eb58db76344f073e40cab29ea5f31b9aafd324
fuzz88 <ivan@oschepkov.ru>
2025-05-27 23:22:00 +0800

    [day8] solved

--- day8/p1/main.rs
+++ day8/p1/main.rs
@@ -7,7 +7,14 @@ fn main() {
 
     let commands = read_input(&input_file);
 
-    println!("{:#?}", &commands);
+    let mut screen = Screen::new(50, 6);
+
+    for command in &commands {
+        screen.process(&command);
+    }
+
+    screen.print();
+    println!("{}", screen.lit_count());
 }
 
 fn read_input(filename: &str) -> Vec<Command> {
@@ -25,8 +32,8 @@ fn parse_command(line: &str) -> Command {
         assert!(components[0] == "rect");
 
         let params: Vec<&str> = components[1].split("x").collect();
-        let width: u32 = params[0].parse().unwrap();
-        let height: u32 = params[1].parse().unwrap();
+        let width = params[0].parse().unwrap();
+        let height = params[1].parse().unwrap();
 
         return Command::Rect { width, height };
     }
@@ -51,6 +58,54 @@ fn parse_command(line: &str) -> Command {
     unreachable!();
 }
 
+struct Screen {
+    pixels: Vec<char>,
+    width: usize,
+    height: usize,
+}
+
+impl Screen {
+    fn new(width: usize, height: usize) -> Screen {
+        let pixels = vec!['.'; width * height];
+        Screen {
+            pixels,
+            width,
+            height,
+        }
+    }
+
+    fn print(&self) {
+        let mut idx = 0;
+        loop {
+            if idx == self.width * self.height {
+                break;
+            }
+            if (idx + 1) % self.width == 0 {
+                print!("{}\n", self.pixels[idx]);
+            } else {
+                print!("{} ", self.pixels[idx]);
+            }
+            idx += 1;
+        }
+        println!();
+    }
+
+    fn process(&mut self, cmd: &Command) {
+        cmd.apply(self);
+    }
+
+    fn lit_count(&self) -> u32 {
+        self.pixels
+            .iter()
+            .map(|pixel| match pixel {
+                '.' => 0,
+                '#' => 1,
+                _ => unreachable!(),
+            })
+            .sum()
+    }
+}
+
 #[derive(Debug)]
 enum Direction {
     Column,
@@ -60,12 +115,66 @@ enum Direction {
 #[derive(Debug)]
 enum Command {
     Rect {
-        width: u32,
-        height: u32,
+        width: usize,
+        height: usize,
     },
     Rotate {
         direction: Direction,
-        index: u32,
-        count: u32,
+        index: usize,
+        count: usize,
     },
+}
+
+impl Command {
+    fn apply(&self, screen: &mut Screen) {
+        match self {
+            Command::Rect { width, height } => {
+                for shift in 0..*height {
+                    for idx in 0..*width {
+                        screen.pixels[shift * screen.width + idx] = '#';
+                    }
+                }
+            }
+            Command::Rotate {
+                direction,
+                index,
+                count,
+            } => {
+                match direction {
+                    Direction::Column => {
+                        let mut n = *count;
+                        while n != 0 {
+                            let mut tmp_pixels: Vec<char> = vec![];
+                            //saving pixels for shifting
+                            for shift in 0..screen.height {
+                                tmp_pixels.push(screen.pixels[shift * screen.width + index]);
+                            }
+                            //shift saved pixels so they dont overwrite themselvs
+                            for shift in 0..screen.height {
+                                let shift2 = (shift + 1) % screen.height;
+                                screen.pixels[shift2 * screen.width + index] = tmp_pixels[shift];
+                            }
+                            //shift `count` times
+                            n -= 1;
+                        }
+                    }
+                    Direction::Row => {
+                        let mut n = *count;
+                        while n != 0 {
+                            let mut tmp_pixels: Vec<char> = vec![];
+                            for idx in 0..screen.width {
+                                tmp_pixels.push(screen.pixels[index * screen.width + idx]);
+                            }
+
+                            for idx in 0..screen.width {
+                                let idx2 = (idx + 1) % screen.width;
+                                screen.pixels[index * screen.width + idx2] = tmp_pixels[idx];
+                            }
+                            n -= 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
