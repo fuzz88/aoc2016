e3737f466b10c1904acac9407f01056b8fe57c83
fuzz88 <ivan@oschepkov.ru>
2025-05-27 20:01:02 +0800

    [day8] add input data

--- day8/p1/main.rs
+++ day8/p1/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("hello, world!");
+    println!("--- Day8: Two-Factor Authentication ---");
+
+    let input_file = std::env::args()
+        .nth(1)
+        .expect("expecting input file as command line argument");
+
+    let commands = read_input(&input_file);
+}
+
+fn read_input(filename: &str) -> Vec<Command> {
+    vec![]
+}
+
+enum Command {
+    Rect {
+        width: u32,
+        height: u32,
+    },
+    Rotate {
+        direction: bool,
+        index: u32,
+        count: u32,
+    },
 }
