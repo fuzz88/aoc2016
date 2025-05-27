2a66ce58ebe625c83736375b15ef5a3082b6627b
fuzz88 <ivan@oschepkov.ru>
2025-05-27 20:37:45 +0800

    [day8] reading input

--- day8/p1/main.rs
+++ day8/p1/main.rs
@@ -6,19 +6,65 @@ fn main() {
         .expect("expecting input file as command line argument");
 
     let commands = read_input(&input_file);
+
+    println!("{:#?}", &commands);
 }
 
 fn read_input(filename: &str) -> Vec<Command> {
-    vec![]
+    std::fs::read_to_string(filename)
+        .expect("expecting valid utf-8 text file")
+        .lines()
+        .map(|line| parse_command(line))
+        .collect()
 }
 
+fn parse_command(line: &str) -> Command {
+    let components: Vec<&str> = line.split_whitespace().collect();
+
+    if components.len() == 2 {
+        assert!(components[0] == "rect");
+
+        let params: Vec<&str> = components[1].split("x").collect();
+        let width: u32 = params[0].parse().unwrap();
+        let height: u32 = params[1].parse().unwrap();
+
+        return Command::Rect { width, height };
+    }
+
+    if components.len() == 5 {
+        assert!(components[0] == "rotate");
+
+        let direction = match components[1] {
+            "column" => Direction::Column,
+            "row" => Direction::Row,
+            _ => unreachable!(),
+        };
+        let index = components[2].split("=").nth(1).unwrap().parse().unwrap();
+        let count = components[4].parse().unwrap();
+
+        return Command::Rotate {
+            direction,
+            index,
+            count,
+        };
+    }
+    unreachable!();
+}
+
+#[derive(Debug)]
+enum Direction {
+    Column,
+    Row,
+}
+
+#[derive(Debug)]
 enum Command {
     Rect {
         width: u32,
         height: u32,
     },
     Rotate {
-        direction: bool,
+        direction: Direction,
         index: u32,
         count: u32,
     },
