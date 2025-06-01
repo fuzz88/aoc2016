fc6a09bf1872a7a10c444fe666351e1f5deab455
fuzz88 <ivan@oschepkov.ru>
2025-06-01 17:19:42 +0800

    [day9] p1 solved

--- day9/p1/main.rs
+++ day9/p1/main.rs
@@ -1,3 +1,5 @@
+#![feature(ascii_char)]
+
 fn main() {
     println!("--- Day 9: Explosives in Cyberspace ---");
 
@@ -12,5 +14,47 @@ fn read_input(filename: &str) -> String {
 }
 
 fn decompressed_len(compressed: &str) -> u32 {
-    compressed.len() as u32
+    let mut idx: usize = 0;
+    let mut result: u32 = 0;
+    let mut in_exp: bool = false;
+    let mut exp_start: usize = 0;
+    let mut exp_end;
+
+    let compressed = compressed.replace(" ", "");
+    let compressed = compressed
+        .as_ascii()
+        .expect("expecting input data to be ascii");
+
+    loop {
+        let ch: char = compressed[idx].into();
+
+        match ch {
+            '(' => {
+                in_exp = true;
+                exp_start = idx + 1;
+            }
+            ')' => {
+                in_exp = false;
+                exp_end = idx - 1;
+                let mut components = compressed[exp_start..=exp_end].as_str().split("x");
+                let (count, repeat) = (
+                    components.next().unwrap().parse::<u32>().unwrap(),
+                    components.next().unwrap().parse::<u32>().unwrap(),
+                );
+                println!("{} {}", count, repeat);
+                idx += count as usize;
+                result += count * repeat;
+            }
+            _ => {
+                if !in_exp {
+                    result += 1;
+                }
+            }
+        }
+
+        idx += 1;
+        if idx == compressed.len() - 1 {
+            break result;
+        }
+    }
 }
