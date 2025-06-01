9bcff7f0640f1bf5436d3a4449e0ea90a6679c30
fuzz88 <ivan@oschepkov.ru>
2025-06-01 16:04:59 +0800

    reading input

--- day9/p1/main.rs
+++ day9/p1/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("hello, world");
+    println!("--- Day 9: Explosives in Cyberspace ---");
+
+    let input_file = std::env::args().nth(1).expect("expecting input file name");
+    let input_data = read_input(&input_file);
+
+    println!("{}", decompressed_len(&input_data));
+}
+
+fn read_input(filename: &str) -> String {
+    std::fs::read_to_string(filename).expect("expecting valid input data")
+}
+
+fn decompressed_len(compressed: &str) -> u32 {
+    compressed.len() as u32
 }
