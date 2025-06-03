12ee71613108cf32100e650744be8d6cdb3f456a
fuzz88 <ivan@oschepkov.ru>
2025-06-03 23:05:32 +0800

    [day10] parsing bot initial states

--- day10/p1/main.rs
+++ day10/p1/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("--- Day 10: Balance Bots ---");
 
@@ -12,14 +14,55 @@ fn main() {
 
 type Output = Vec<u32>; // Output is an array of microchips.
 
-type Bot = [u32; 2]; // Bot is basically described as a two microchips it holds.
-type Bots = Vec<Bot>;
+type Bot = [Option<u32>; 2]; // Bot is basically described as a two microchips it holds.
+type Bots = HashMap<u32, Bot>;
+
+fn give_microchip(chip: u32, bot: &mut Bot) {
+    if bot[0] == None {
+        bot[0] = Some(chip);
+        return;
+    } else if bot[1] == None {
+        bot[1] = Some(chip);
+        return;
+    }
+    unreachable!("cant give to bot, it already has two microchips. wrong input");
+}
+
+fn take_higher(bot: &mut Bot) -> u32 {
+    if bot[0] != None && bot[1] != None {
+        if bot[0] < bot[1] {
+            let higher = bot[1];
+            bot[1] = None;
+            return higher.unwrap();
+        } else {
+            let higher = bot[0];
+            bot[0] = None;
+            return higher.unwrap();
+        }
+    }
+    unreachable!("cant take from bot with only one microchip. wrong input");
+}
+
+fn take_lower(bot: &mut Bot) -> u32 {
+    if bot[0] != None && bot[1] != None {
+        if bot[0] > bot[1] {
+            let lower = bot[1];
+            bot[1] = None;
+            return lower.unwrap();
+        } else {
+            let lower = bot[0];
+            bot[0] = None;
+            return lower.unwrap();
+        }
+    }
+    unreachable!("cant take from bot with only one microchip. wrong input");
+}
 
 type Rule = ();
 type Rules = Vec<Rule>;
 
 fn read_input(filename: &str) -> (Bots, Rules) {
-    let mut bots: Bots = vec![];
+    let mut bots: Bots = HashMap::new();
     let mut rules: Rules = vec![];
 
     std::fs::read_to_string(filename)
@@ -28,9 +71,16 @@ fn read_input(filename: &str) -> (Bots, Rules) {
         .for_each(|line| {
             if line.starts_with("v") {
                 // "value N goes to ..."
-                bots.push(parse_bot(line));
+                let (value, bot_number) = parse_bot(line);
+                if let Some(bot) = bots.get_mut(&bot_number) {
+                    give_microchip(value, bot);
+                } else {
+                    let mut new_bot = [None; 2];
+                    give_microchip(value, &mut new_bot);
+                    bots.insert(bot_number, new_bot);
+                }
             } else {
-                // "bot 0 gives ..."
+                // "bot N gives ..."
                 rules.push(parse_rule(line));
             }
         });
@@ -38,10 +88,16 @@ fn read_input(filename: &str) -> (Bots, Rules) {
     (bots, rules)
 }
 
-fn parse_bot(line: &str) -> Bot {
-    [1, 2]
+fn parse_bot(line: &str) -> (u32, u32) {
+    println!("bot: {}", line);
+    let components: Vec<&str> = line.split_whitespace().collect();
+    (
+        components[1].parse().unwrap(),
+        components[5].parse().unwrap(),
+    )
 }
 
 fn parse_rule(line: &str) -> Rule {
+    println!("rule: {}", line);
     ()
 }
