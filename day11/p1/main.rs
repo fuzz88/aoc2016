3f7b109888240782779c195f45012b69560b8e59
fuzz88 <ivan@oschepkov.ru>
2025-06-05 15:21:29 +0800

    [day11] input in progress

--- day11/p1/main.rs
+++ day11/p1/main.rs
@@ -1,15 +1,28 @@
 use std::env;
+use std::fs;
 
-fn main() -> Result<(), &'static str> {
+type Result<T> = std::result::Result<T, String>;
+
+fn main() -> Result<()> {
     println!("--- Day 11: Radioisotope Thermoelectric Generators ---");
 
     let input_file = env::args()
         .nth(1)
-        .ok_or("no input file provided")?;
+        .ok_or("no input filename as cli argument")?;
 
     println!("{}", input_file);
 
+    let input_data = read_input(&input_file);
 
+    println!("{:?}", input_data);
 
     Ok(())
+}
+
+fn read_input(filename: &str) -> Result<Vec<String>> {
+    Ok(fs::read_to_string(filename)
+        .map_err(|err| format!("can't read from input file {filename}: {err}"))?
+        .lines()
+        .map(|line| line.to_owned())
+        .collect())
 }
