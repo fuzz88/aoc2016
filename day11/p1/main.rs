ae0bb3488b4d2b7c91ed4250bf407755dea95969
fuzz88 <ivan@oschepkov.ru>
2025-06-05 13:35:43 +0800

    [day11] input.txt and error handling

--- day11/p1/main.rs
+++ day11/p1/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("hello, world");
+use std::env;
+
+fn main() -> Result<(), &'static str> {
+    println!("--- Day 11: Radioisotope Thermoelectric Generators ---");
+
+    let input_file = env::args()
+        .nth(1)
+        .ok_or("no input file provided")?;
+
+    println!("{}", input_file);
+
+
+
+    Ok(())
 }
