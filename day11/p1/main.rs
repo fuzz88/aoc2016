5b4ba8aa4bdd86a08045aa5be1f44f62f744aec1
fuzz88 <ivan@oschepkov.ru>
2025-06-06 14:48:08 +0800

    [day11] paused

--- day11/p1/main.rs
+++ day11/p1/main.rs
@@ -2,10 +2,17 @@ use std::env;
 use std::fs;
 use std::io;
 
-#[derive(Debug, Clone)]
-enum Item {
-    Microchip { element: String, floor: u32 },
-    Generator { element: String, floor: u32 },
+#[derive(Debug)]
+enum ItemType {
+    Microchip,
+    Generator,
+}
+
+#[derive(Debug)]
+struct Item {
+    item_type: ItemType,
+    element: String,
+    floor: u32,
 }
 
 fn main() -> Result<(), io::Error> {
@@ -17,10 +24,49 @@ fn main() -> Result<(), io::Error> {
     ))?;
 
     let input_data = read_input(&input_file)?;
-
     println!("{:#?}", input_data);
 
+    println!("{}", solver(&input_data));
+
     Ok(())
+}
+
+fn solver(items: &Vec<Item>) -> u32 {
+    let mut solutions_step_count: Vec<u32> = vec![];
+
+    solutions_step_count.push(99);
+    // some way of searching.
+    // some combinatorics.
+    //
+    // how much operations needed to move all items, if there is no limitations?
+    //
+    // limitations:
+    //
+    //      1. both generator and microchip are blockers, and this adds operations.
+    //      2. elevator must not be empty, and this adds operations.
+    //
+    //
+    // there is some kind of formula:
+    //
+    // min_ops = ops_without_limitations + ops_to_keep_invariants
+    //
+    // and ops_to_keep_invariants depends of relative positions of blockers and elevator mechanics.
+
+    *solutions_step_count
+        .iter()
+        .max()
+        .expect("expecting at least one solution to be found")
+}
+
+fn take_elevator(items: Vec<Item>, idxs_to_move: [usize; 2], end_floor: u32) -> bool {
+    true
+}
+
+fn floor_items(items: Vec<Item>, floor: u32) -> Vec<Item> {
+    items
+        .into_iter()
+        .filter(|item| item.floor == floor)
+        .collect()
 }
 
 fn read_input(filename: &str) -> Result<Vec<Item>, io::Error> {
@@ -37,18 +83,20 @@ fn parse_line(line: &str) -> Vec<Item> {
     let mut items: Vec<Item> = vec![];
     let components: Vec<&str> = line.split_whitespace().collect();
 
-    // always starting from the first floor
+    // always starting from the first floor.
     let mut floor: u32 = 1;
     // parse floor information by iterating pairs of tokens.
     components.windows(2).for_each(|pair| match pair {
         ["The", "second"] => floor = 2,
         ["The", "third"] => floor = 3,
         ["The", "fourth"] => floor = 4,
-        [element, "generator," | "generator." | "generator"] => items.push(Item::Generator {
+        [element, "generator," | "generator." | "generator"] => items.push(Item {
+            item_type: ItemType::Generator,
             element: element.to_string(),
             floor,
         }),
-        [element, "microchip," | "microchip." | "microchip"] => items.push(Item::Microchip {
+        [element, "microchip," | "microchip." | "microchip"] => items.push(Item {
+            item_type: ItemType::Microchip,
             element: element
                 .split("-")
                 .nth(0)
