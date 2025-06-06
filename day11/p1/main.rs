fad0f7e0d61b1bd09c59310fb6fa9d5f04690a8c
fuzz88 <ivan@oschepkov.ru>
2025-06-06 12:05:09 +0800

    [day11] reading input

--- day11/p1/main.rs
+++ day11/p1/main.rs
@@ -3,6 +3,12 @@ use std::fs;
 
 type Result<T> = std::result::Result<T, String>;
 
+#[derive(Debug, Clone)]
+enum Item {
+    Microchip { element: String, floor: u32 },
+    Generator { element: String, floor: u32 },
+}
+
 fn main() -> Result<()> {
     println!("--- Day 11: Radioisotope Thermoelectric Generators ---");
 
@@ -10,19 +16,46 @@ fn main() -> Result<()> {
         .nth(1)
         .ok_or("no input filename as cli argument")?;
 
-    println!("{}", input_file);
-
     let input_data = read_input(&input_file);
 
-    println!("{:?}", input_data);
+    println!("{:#?}", input_data);
 
     Ok(())
 }
 
-fn read_input(filename: &str) -> Result<Vec<String>> {
-    Ok(fs::read_to_string(filename)
-        .map_err(|err| format!("can't read from input file {filename}: {err}"))?
+fn read_input(filename: &str) -> Result<Vec<Item>> {
+    let items = fs::read_to_string(filename)
+        .map_err(|err| format!("`{filename}`: {err}"))?
         .lines()
-        .map(|line| line.to_owned())
-        .collect())
+        .map(|line| parse_line(line))
+        .collect::<Vec<Vec<Item>>>()
+        .into_iter()
+        .flatten()
+        .collect();
+
+    Ok(items)
+}
+
+fn parse_line(line: &str) -> Vec<Item> {
+    let mut items: Vec<Item> = vec![];
+    let components: Vec<&str> = line.split_whitespace().collect();
+
+    // always starting from the first floor
+    let mut floor: u32 = 1;
+    components.windows(2).for_each(|pair| match pair {
+        ["The", "second"] => floor = 2,
+        ["The", "third"] => floor = 3,
+        ["The", "fourth"] => floor = 4,
+        [element, "generator," | "generator." | "generator"] => items.push(Item::Generator {
+            element: element.to_string(),
+            floor,
+        }),
+        [element, "microchip," | "microchip." | "microchip"] => items.push(Item::Microchip {
+            element: element.split("-").nth(0).unwrap().to_string(),
+            floor,
+        }),
+        _ => {}
+    });
+
+    items
 }
