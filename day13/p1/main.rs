8c49a9886139c044cbfe78b59c76f6ea947d3237
fuzz88 <ivan@oschepkov.ru>
2025-06-12 13:31:37 +0800

    [day13] formatting

--- day13/p1/main.rs
+++ day13/p1/main.rs
@@ -1,8 +1,8 @@
+#[rustfmt::skip]
 fn is_wall(x: u32, y: u32) -> bool {
     // designers favourite number
     let dfn = 1358;
-    // x^2 + 3x + 2xy + y + y^2
-    let n = x * x + 3 * x + 2 * x * y + y + y * y;
+    let n = x*x + 3*x + 2*x*y + y + y*y;
 
     (n + dfn).count_ones() % 2 == 0
 }
