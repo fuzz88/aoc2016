22f223003fb5f6ed1aef3ab692c79bd722cadcc7
fuzz88 <ivan@oschepkov.ru>
2025-06-12 11:43:24 +0800

    [day12] cleanup

--- day12/p1/main.rs
+++ day12/p1/main.rs
@@ -32,7 +32,7 @@ enum Instruction {
     CPY { value: Op, dst: Op },
     INC(Op),
     DEC(Op),
-    JNZ { cond: Op, count: i32 },
+    JNZ { cond: Op, count: Op },
 }
 
 struct Parser {
@@ -54,7 +54,10 @@ impl Parser {
             curr: 0,
         })
     }
-    fn parse_operand(name: &str) -> Op {
+
+    fn parse_next_token_as_operand(&mut self) -> Op {
+        let name = self.get_next_token();
+
         match name {
             "a" => Op::Register(Reg::A),
             "b" => Op::Register(Reg::B),
@@ -63,45 +66,33 @@ impl Parser {
             num => Op::Number(num.parse().unwrap()),
         }
     }
+
+    fn get_next_token(&mut self) -> &str {
+        self.curr += 1;
+        self.input_data[self.curr - 1].as_ref()
+    }
 }
 
 impl Iterator for Parser {
     type Item = Instruction;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.curr += 1;
-        if self.curr < self.input_data.len() {
-            Some(match self.input_data[self.curr - 1].as_ref() {
-                "cpy" => {
-                    self.curr += 2;
-                    Instruction::CPY {
-                        value: Parser::parse_operand(self.input_data[self.curr - 2].as_ref()),
-                        dst: Parser::parse_operand(self.input_data[self.curr - 1].as_ref()),
-                    }
-                }
-                "inc" => {
-                    self.curr += 1;
-                    Instruction::INC(Parser::parse_operand(
-                        self.input_data[self.curr - 1].as_ref(),
-                    ))
-                }
-                "dec" => {
-                    self.curr += 1;
-                    Instruction::DEC(Parser::parse_operand(
-                        self.input_data[self.curr - 1].as_ref(),
-                    ))
-                }
-                "jnz" => {
-                    self.curr += 2;
-                    Instruction::JNZ {
-                        cond: Parser::parse_operand(self.input_data[self.curr - 2].as_ref()),
-                        count: self.input_data[self.curr - 1].parse().unwrap(),
-                    }
-                }
+        if self.curr == self.input_data.len() {
+            None
+        } else {
+            Some(match self.get_next_token() {
+                "cpy" => Instruction::CPY {
+                    value: self.parse_next_token_as_operand(),
+                    dst: self.parse_next_token_as_operand(),
+                },
+                "inc" => Instruction::INC(self.parse_next_token_as_operand()),
+                "dec" => Instruction::DEC(self.parse_next_token_as_operand()),
+                "jnz" => Instruction::JNZ {
+                    cond: self.parse_next_token_as_operand(),
+                    count: self.parse_next_token_as_operand(),
+                },
                 _ => unreachable!("no other instruction types"),
             })
-        } else {
-            None
         }
     }
 }
@@ -120,44 +111,44 @@ impl Machine {
     }
 
     fn run(&mut self, program: &Vec<Instruction>) {
-        loop {
-            if self.pc >= program.len() {
-                break;
-            } else {
-                match &program[self.pc] {
-                    Instruction::INC(op) => match op {
-                        Op::Number(_) => {}
-                        Op::Register(reg) => self.registers[reg.idx()] += 1,
-                    },
-                    Instruction::DEC(op) => match op {
-                        Op::Number(_) => {}
-                        Op::Register(reg) => self.registers[reg.idx()] -= 1,
-                    },
-                    Instruction::CPY { value, dst } => match dst {
-                        Op::Number(_) => {}
-                        Op::Register(reg) => {
-                            self.registers[reg.idx()] = match value {
-                                Op::Register(reg) => self.registers[reg.idx()],
-                                Op::Number(value) => *value,
+        while self.pc < program.len() {
+            match &program[self.pc] {
+                Instruction::INC(op) => match op {
+                    Op::Number(_) => {}
+                    Op::Register(reg) => self.registers[reg.idx()] += 1,
+                },
+                Instruction::DEC(op) => match op {
+                    Op::Number(_) => {}
+                    Op::Register(reg) => self.registers[reg.idx()] -= 1,
+                },
+                Instruction::CPY { value, dst } => match dst {
+                    Op::Number(_) => {}
+                    Op::Register(reg) => {
+                        self.registers[reg.idx()] = match value {
+                            Op::Register(reg) => self.registers[reg.idx()],
+                            Op::Number(value) => *value,
+                        }
+                    }
+                },
+                Instruction::JNZ { cond, count } => match cond {
+                    Op::Register(reg) => {
+                        if self.registers[reg.idx()] != 0 {
+                            if let Op::Number(count) = count {
+                                self.pc = (self.pc as i32 + count - 1) as usize;
                             }
-                        }
-                    },
-                    Instruction::JNZ { cond, count } => match cond {
-                        Op::Register(reg) => {
-                            if self.registers[reg.idx()] != 0 {
+                        };
+                    }
+                    Op::Number(value) => {
+                        if *value != 0 {
+                            if let Op::Number(count) = count {
                                 self.pc = (self.pc as i32 + count - 1) as usize;
-                            };
-                        }
-                        Op::Number(value) => {
-                            if *value != 0 {
-                                self.pc = (self.pc as i32 + count - 1) as usize;
-                            };
-                        }
-                    },
-                }
-                // println!("{} {:?}", self.pc, self.registers);
-                self.pc += 1;
+                            }
+                        };
+                    }
+                },
             }
+            // println!("{} {:?}", self.pc, self.registers);
+            self.pc += 1;
         }
     }
 }
@@ -167,7 +158,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let input_file = env::args()
         .nth(1)
-        .expect("expecting input file name as cli argument");
+        .ok_or("expecting input file name as cli argument")?;
 
     let program = Parser::tokenize(&input_file)?.collect::<Vec<Instruction>>();
 
@@ -176,7 +167,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", machine.registers[Reg::A.idx()]);
 
     let mut machine = Machine::new();
-    machine.registers[2] = 1;
+    machine.registers[Reg::C.idx()] = 1;
     machine.run(&program);
     println!("{}", machine.registers[Reg::A.idx()]);
 
