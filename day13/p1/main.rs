6bf12a763f9e756e9bd328cc7e597b5e59e66b89
fuzz88 <ivan@oschepkov.ru>
2025-06-12 13:26:00 +0800

    [day13] is_wall

--- day13/p1/main.rs
+++ day13/p1/main.rs
@@ -1,3 +1,12 @@
+fn is_wall(x: u32, y: u32) -> bool {
+    // designers favourite number
+    let dfn = 1358;
+    // x^2 + 3x + 2xy + y + y^2
+    let n = x * x + 3 * x + 2 * x * y + y + y * y;
+
+    (n + dfn).count_ones() % 2 == 0
+}
+
 fn main() {
-    println!("hello, world");
+    println!("--- Day 13: A Maze of Twisty Little Cibicles ---");
 }
