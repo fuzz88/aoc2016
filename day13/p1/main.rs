58a5952915b9fce1d3dac6e58bcae6b18e8a4654
fuzz88 <ivan@oschepkov.ru>
2025-06-12 17:43:10 +0800

    [day13] solved

--- day13/p1/main.rs
+++ day13/p1/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::collections::{HashSet, VecDeque};
 
 #[rustfmt::skip]
@@ -8,12 +9,26 @@ fn make_is_wall(dfn: i32) -> impl Fn(i32, i32) -> bool {
     }
 }
 
-fn search_path(x: i32, y: i32, xd: i32, yd: i32, is_wall: impl Fn(i32, i32) -> bool) {
+#[rustfmt::skip]
+fn search_path_and_count_dist(
+    x: i32,                                 // start coordinates
+    y: i32,
+    xd: i32,                                // destination coordinates
+    yd: i32,
+    max_dist: u32,                          // how far can we reach
+    is_wall: impl Fn(i32, i32) -> bool,
+) -> (u32, u32) {
     // bfs
+    // hope it will stop on this infinite maze
+
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
     let mut for_processing: VecDeque<(i32, i32, u32)> = VecDeque::new();
 
+    let mut min_dist = 1_000_000;
+    let mut count = 0;
+
     for_processing.push_back((x, y, 0));
+    visited.insert((x, y));
 
     while !for_processing.is_empty() {
         let Some((x, y, dist)) = for_processing.pop_front() else {
@@ -21,27 +36,36 @@ fn search_path(x: i32, y: i32, xd: i32, yd: i32, is_wall: impl Fn(i32, i32) -> b
         };
 
         if x == xd && y == yd {
-            println!("{}", dist);
-            break;
+            min_dist = min(dist, min_dist);
         }
-
-        visited.insert((x, y));
+        if dist <= max_dist {
+            // println!("{} {}", x, y);
+            count += 1;
+        }
 
         let dxs = (-1..=1).flat_map(|i| (-1..=1).map(move |j| (i, j)));
 
-        dxs.filter(|(dx, dy)| *dx == 0 || *dy == 0) // no diagonal movement
-            .map(|(dx, dy)| (x + dx, y + dy)) // neighbours
-            .filter(|(nx, ny)| {
-                // filter out negatives, walls and visited
-                *nx >= 0 && *ny >= 0 && !is_wall(*nx, *ny) && !visited.contains(&(*nx, *ny))
+        dxs.filter(|(dx, dy)| *dx == 0 || *dy == 0)     // no diagonal movement
+            .map(|(dx, dy)| (x + dx, y + dy))           // neighbours
+            .filter(|(nx, ny)| {                        // filter out negatives and walls
+                *nx >= 0 && *ny >= 0 && !is_wall(*nx, *ny)
             })
-            .for_each(|(nx, ny)| for_processing.push_back((nx, ny, dist + 1)));
+            .for_each(|(nx, ny)| {
+                if !visited.contains(&(nx, ny)) {       // filter out already visited
+                    visited.insert((nx, ny));
+                    for_processing.push_back((nx, ny, dist + 1));
+                }
+            });
     }
+
+    (min_dist, count)
 }
 
 fn main() {
     println!("--- Day 13: A Maze of Twisty Little Cibicles ---");
 
-    search_path(1, 1, 31, 39, make_is_wall(1358));
-    // search_path(1, 1, 7, 4, make_is_wall(10));
+    println!(
+        "{:?}",
+        search_path_and_count_dist(1, 1, 31, 39, 50, make_is_wall(1358))
+    );
 }
