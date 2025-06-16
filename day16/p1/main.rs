a8ee41a625a8af3b47a3c3c6fc9f35b2b0f12ed7
fuzz88 <ivan@oschepkov.ru>
2025-06-16 22:49:38 +0800

    [day16] solved

--- day16/p1/main.rs
+++ day16/p1/main.rs
@@ -1,3 +1,4 @@
+#![feature(iter_array_chunks)]
 use std::env;
 use std::fs;
 
@@ -10,11 +11,60 @@ fn main() -> Result<()> {
         .nth(1)
         .ok_or("no input file is provided as cli argument")?;
 
-    let input_data = read_input(&input_file);
+    let input_data = read_input(&input_file)?;
 
-    println!("{input_data:?}");
+    let checksum = calculate_checksum(input_data.0, input_data.1);
+
+    println!("{checksum}");
 
     Ok(())
+}
+
+fn calculate_checksum(disk_length: usize, mut initial_state: String) -> String {
+    let mut result = String::new();
+
+    loop {
+        result.clear();
+        let a = initial_state;
+
+        result.push_str(&a);
+        result.push('0');
+
+        for ch in a.chars().rev() {
+            result.push(match ch {
+                '0' => '1',
+                '1' => '0',
+                _ => unreachable!("input consists of 0 and 1"),
+            });
+        }
+
+        if result.len() >= disk_length {
+            break;
+        } else {
+            initial_state = result.clone();
+        };
+    }
+
+    result = result[..disk_length].to_string();
+    let mut checksum: String;
+
+    loop {
+        checksum = result
+            .chars()
+            .array_chunks()
+            .map(|[a, b]| match a == b {
+                true => '1',
+                false => '0',
+            })
+            .collect();
+
+        if checksum.len() % 2 != 0 {
+            break;
+        }
+        result = checksum;
+    }
+
+    checksum
 }
 
 fn read_input(filename: &str) -> Result<(usize, String)> {
