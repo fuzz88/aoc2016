166aee8b96d8fa34a030780466f39d3a2d4244d8
fuzz88 <ivan@oschepkov.ru>
2025-06-17 19:34:04 +0800

    [day19] p1 solved

--- day19/p1/main.rs
+++ day19/p1/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("hello, world");
+    println!("--- Day 19: An Elephant Named Joseph ---");
+
+    let elf_count = 3018458;
+    println!("{}", winner(elf_count));
+}
+
+fn winner(n: u32) -> u32 {
+    1 + (n - u32::pow(2, (n.ilog2() as f32).floor() as u32)) * 2
 }
