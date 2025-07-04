708282e16003a06e47ed902436b9e857be7537e5
fuzz88 <ivan@oschepkov.ru>
2025-07-04 21:54:55 +0800

    [day11] maybe better data representation

--- day11/p1/main.rs
+++ day11/p1/main.rs
@@ -3,13 +3,13 @@ use std::fmt;
 use std::fs;
 use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Ord, Eq, PartialEq, PartialOrd)]
 enum ItemType {
-    Microchip,
     Generator,
+    Microchip,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Item {
     item_type: ItemType,
     element: String,
@@ -27,67 +27,32 @@ fn main() -> Result<(), io::Error> {
     let mut input_data = read_input(&input_file)?;
     println!("{:#?}", input_data);
 
-    elevator(&mut input_data, [Some(0), Some(1)], 2);
-
-    println!("{:#?}", input_data);
+    println!("{:#?}", items_to_vec(&input_data));
 
     // println!("{}", solver(&input_data));
 
     Ok(())
 }
 
-fn solver(items: &Vec<Item>) -> u32 {
-    let mut solutions_step_count: Vec<u32> = vec![];
+fn items_to_vec(items: &Vec<Item>) -> Vec<u32> {
+    let mut results = vec![];
+    let mut items = items.clone();
+    items.sort_by_key(|item| item.item_type.clone());
+    items.sort_by_key(|item| item.element.clone());
+    loop {
+        if items.is_empty() {
+            break;
+        }
 
-    solutions_step_count.push(99);
-    //
-    // some way of searching.
-    // some combinatorics.
-    //
-    // how many operations are needed to move all items, if there are no limitations?
-    //
-    // limitations:
-    //
-    //      1. both generator and microchip are blockers, and this adds operations.
-    //      2. elevator must not be empty, and this adds operations.
-    //
-    //
-    // intuition tells me that there is kind of formula:
-    //
-    // min_ops = ops_without_limitations + ops_to_keep_invariants
-    //
-    // and ops_to_keep_invariants depends of relative positions of blockers and elevator mechanics.
-    //
-    // but what if we want actual sequence of operations?
-    // how to implement full enumeration of operational sequences which are leading to desired
-    // results? how to implement branch pruning and backtracking of variants?
-    //
+        let item1 = items.pop().unwrap();
+        let item2 = items.pop().unwrap();
 
-    *solutions_step_count
-        .iter()
-        .max()
-        .expect("expecting at least one solution to be found")
-}
-
-fn elevator(items: &mut Vec<Item>, idxs: [Option<usize>; 2], end_floor: u32) -> bool { 
-    // takes two or one item and tries to move it to target floor
-    if let Some(id1) = idxs[0] {
-        let item1 = items.get_mut(id1).unwrap();
-        item1.floor = end_floor;
+        results.push(item1.floor);
+        results.push(item2.floor);
     }
-    if let Some(id2) = idxs[1] {
-        let item2 = items.get_mut(id2).unwrap();
-        item2.floor = end_floor;
-    }
-    true
-}
 
-// fn floor_items(items: &Vec<Item>, floor: u32) -> Vec<Item> {
-//     items
-//         .into_iter()
-//         .filter(|item| item.floor == floor)
-//         .collect()
-// }
+    results
+}
 
 fn read_input(filename: &str) -> Result<Vec<Item>, io::Error> {
     let items = fs::read_to_string(filename)?
